bd8a2b4278b971c23314d1e22219c55f77d3c9e5
will.renoulleau <wil.renou@gmail.com>
2023-03-06 12:04:14 +0100

    Commit de toutes les vues crées avec amour

---
+++ app/src/main/java/com/example/projetimmobill/controleur/ConsultLocationActivity.java
@@ -0,0 +1,16 @@
+package com.example.projetimmobill.controleur;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.projetimmobill.R;
+
+public class ConsultLocationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_consult_location);
+    }
+}
--- app/src/main/java/com/example/projetimmobill/controleur/DetailsLocataireActivity.java
+++ app/src/main/java/com/example/projetimmobill/controleur/DetailsLocataireActivity.java
@@ -3,14 +3,14 @@ package com.example.projetimmobill.controleur;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
 import com.example.projetimmobill.R;
 
-public class ConnexionActivity extends AppCompatActivity {
-
+public class DetailsLocataireActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_connexion);
+        setContentView(R.layout.activity_details_locataire);
     }
 }
