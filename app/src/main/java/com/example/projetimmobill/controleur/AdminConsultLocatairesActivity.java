e01d17922259dd83c4b1612be4a2a63f59927189
clar.noirot <clarence.noirot@gmail.com>
2023-03-13 12:03:51 +0100

    AdminConsultLocatairesActivity listview

--- app/src/main/java/com/example/projetimmobill/controleur/AdminConsultLocatairesActivity.java
+++ app/src/main/java/com/example/projetimmobill/controleur/AdminConsultLocatairesActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,6 +14,8 @@ import android.widget.ListView;
 import com.example.projetimmobill.R;
 import com.example.projetimmobill.modele.Locataire;
 import com.example.projetimmobill.modele.LocataireDAO;
+import com.example.projetimmobill.modele.Villa;
+import com.example.projetimmobill.modele.VillaDAO;
 
 import java.util.ArrayList;
 
@@ -37,14 +40,45 @@ public class AdminConsultLocatairesActivity extends AppCompatActivity {
 
 
         listview=(ListView)findViewById(R.id.listLocataires);
-
-
-
         LocataireDAO locataireDAO=new LocataireDAO(getApplicationContext());
 
         ArrayList<Locataire> ok= locataireDAO.getLocataire();
         ArrayAdapter<Locataire> v=new ArrayAdapter(this,android.R.layout.simple_list_item_1,ok);
         listview.setAdapter(v);
+
+
+
+
+
+
+
+        LocataireDAO resaAcces = new LocataireDAO(this);
+
+        lesLocataires = resaAcces.getLocataire();
+
+        ArrayAdapter monAdapter = new ArrayAdapter(AdminConsultLocatairesActivity.this,android.R.layout.simple_list_item_1,lesLocataires);
+
+        listview.setAdapter(monAdapter);
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                                            @Override
+                                            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                                                Locataire selectedItem = (Locataire) listview.getAdapter().getItem(position);
+                                                Intent i = new Intent(AdminConsultLocatairesActivity.this, AdminDetailsLocataireActivity.class);
+                                                i.putExtra("pos", position);
+                                                i.putExtra("id", selectedItem.getId());
+                                                i.putExtra("nom", selectedItem.getNom());
+                                                i.putExtra("adresse", selectedItem.getAdresse());
+                                                i.putExtra("prenom", selectedItem.getPrenom());
+                                                i.putExtra("tel", selectedItem.getTel());
+                                                i.putExtra("email", selectedItem.getEmail());
+                                                i.putExtra("commentaire", selectedItem.getCommentaire());
+
+                                                startActivity(i);
+                                            }
+                                        });
+
+
 
 
         btnRetour = (Button) findViewById(R.id.btnRetour);
--- app/src/main/java/com/example/projetimmobill/controleur/AdminDetailsLocataireActivity.java
+++ app/src/main/java/com/example/projetimmobill/controleur/AdminDetailsLocataireActivity.java
@@ -6,11 +6,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.projetimmobill.R;
 
 public class AdminDetailsLocataireActivity extends AppCompatActivity {
     private Button btnRetour;
+    private TextView textId, textNom, textAdresse, textPrenom, textTel, textEmail, textCommentaire;
+    private String nom;
+    private int id;
+    private String prenom;
+    private String adresse;
+    private String tel;
+    private String email;
+    private String commentaire;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,5 +34,23 @@ public class AdminDetailsLocataireActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        textId = (TextView) findViewById(R.id.textTitre);
+        textAdresse = (TextView) findViewById(R.id.textView2);
+        textNom = (TextView) findViewById(R.id.textView11);
+        textPrenom = (TextView) findViewById(R.id.textView);
+        textTel = (TextView) findViewById(R.id.textView3);
+        textEmail = (TextView) findViewById(R.id.textView4);
+        textCommentaire = (TextView) findViewById(R.id.textView5);
+
+
+        id=getIntent().getIntExtra("id",0);
+        nom=getIntent().getStringExtra("nom");
+        adresse=getIntent().getStringExtra("adresse");
+        prenom=getIntent().getStringExtra("prenom");
+        tel=getIntent().getStringExtra("tel");
+        email=getIntent().getStringExtra("email");
+        commentaire=getIntent().getStringExtra("commentaire");
+
     }
 }
